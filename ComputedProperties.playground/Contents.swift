008acb579d73a2f10b38e7ed9e246eda8d3047f6
Konstantin <grahovsky@gmail.com>
2019-03-15 20:22:45 +0300

    #computed properties #setter

--- ComputedProperties.playground/Contents.swift
+++ ComputedProperties.playground/Contents.swift
@@ -6,8 +6,13 @@ var numberOfSlices: Int {
     get {
         return pizzaInInches - 4
     }
+    set {
+        print("numberOfSlices now has a new value which is \(newValue)")
+    }
 }
 
 print(numberOfSlices)
+
+numberOfSlices = 15
 
 
