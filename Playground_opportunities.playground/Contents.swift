3502f95c43cb282becac692bdfa0c3eeea347436
Konstantin <grahovsky@gmail.com>
2019-04-28 20:18:22 +0300

    reference comments

--- Playground_opportunities.playground/Contents.swift
+++ Playground_opportunities.playground/Contents.swift
@@ -9,3 +9,21 @@ for count in 200...1000 {
     sin(10000.0/Double(count))
 }
 
+
+//справочные комментарии
+
+/**
+ This func say hello to user
+ - parameter name: String Name of user
+ - returns: Absolutely nothing
+ - throws: Error when name is array
+ - authors: Grahovsky
+ - bug: This is very simple function
+ */
+func sayHello(name: String) {
+    print("hello, \(name)!")
+}
+
+sayHello(name: "Max")
+
+
